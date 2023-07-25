eced54bf7c897bb6e851a86a0247be5d0b29ea19
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-07-25 09:02:14 +0330

    add the game class

---
+++ game.py
@@ -0,0 +1,12 @@
+'''
+game class contains all the components of the game
+including the main map, players, and the game state and turn number
+'''
+
+class Game:
+    def __init__(self) -> None:
+        self.list_of_players = [] # list of Player objects
+        self.list_of_nodes = [] # list of Node objects
+        self.state = None # that could be 'add troops': 1, 'attack': 2, 'move troops': 3
+        self.turn_number = 0 # each turn is a round for a player to play
+        self.player_turn = None # Player object: the player who is playing this turn
