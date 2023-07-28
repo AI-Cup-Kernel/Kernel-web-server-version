d0e82d9ccd09ea8033ff830f6ed313bf9a5c81ce
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-07-28 16:28:57 +0330

    add private key and port for the server

--- src/blueprints/get_token.py
+++ src/blueprints/get_token.py
@@ -4,11 +4,16 @@ from flask import jsonify
 import jwt
 import importlib.util
 
+
 # import the read_config function from tools/read_config.py
 spec = importlib.util.spec_from_file_location('read_config', 'src/tools/read_config.py')
 read_config = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(read_config)
 
+# import main_game instance from components/game.py
+spec = importlib.util.spec_from_file_location('main_game', 'src/components/game.py')
+game = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(game)
 
 # initialize the login blueprint
 login = Blueprint('login', __name__)
@@ -26,12 +31,12 @@ def login_func():
     if player_id > config['max_players']:
         output_dict = {'error': 'You are not allowed to login anymore'}
         return jsonify(output_dict), 403
-    
+
     # create a token for the player
     token = jwt.encode({'player_id': player_id}, current_app.config['SECRET_KEY'], 'HS256')
 
     # create the output dictionary
-    output_dict = {'token': token, 'player_id': player_id}
+    output_dict = {'token': token, 'player_id': player_id, 'public_key': game.main_game.public_key_encoded, 'port': config['client_port_start']+player_id}
 
     return jsonify(output_dict), 200
 
--- src/components/game.py
+++ src/components/game.py
@@ -3,6 +3,8 @@ game class contains all the components of the game
 including the main map, players, and the game state and turn number
 '''
 
+import rsa
+
 class Game:
     def __init__(self) -> None:
         self.list_of_players = [] # list of Player objects
@@ -10,13 +12,21 @@ class Game:
         self.state = None # that could be 'add troops': 1, 'attack': 2, 'move troops': 3
         self.turn_number = 0 # each turn is a round for a player to play
         self.player_turn = None # Player object: the player who is playing this turn
-    
+        # Generate key pair (public key and private key)
+        (public_key, self.private_key) = rsa.newkeys(512)
+        # Encode the public key to send it to the server
+        self.public_key_encoded = public_key.save_pkcs1().decode('utf-8')
+
     def update_component_numbers(self) -> None:
         pass
 
     
     def read_map(self, map_file: str) -> None:
         pass
+
+
+
+main_game = Game()
 
     
     
