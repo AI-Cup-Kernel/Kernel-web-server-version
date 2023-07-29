ae245f9ad763cd9c0dfdcabbba3256145c108476
TRX <trx@TRXs-MacBook-Air.local>
2023-07-29 11:29:44 +0330

    update the files to work correct

--- src/run.py
+++ src/run.py
@@ -1,32 +1,66 @@
 """
-    in this file we will make a server to run the game
+    this file will make a server 
     and add different APIs from blueprints to the server
 """
 
-
-
 from flask import Flask
-from blueprints.index import index
-from blueprints.get_token import login
 import importlib.util
+from components.game import main_game
 
 # import the read_config function from tools/read_config.py
 spec = importlib.util.spec_from_file_location('read_config', 'src/tools/read_config.py')
 read_config = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(read_config)
 
+# import the check_token function from tools/check_token.py
+spec = importlib.util.spec_from_file_location('check_token', 'src/tools/check_token.py')
+check_token = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(check_token)
 
+
+# initialize the flask app
 app = Flask(__name__)
+app.app_context().push()
+
+
 
 # set the secret key
 app.config['SECRET_KEY'] = 'your-secret-key'
+
+# set the main_game instance in the flask global variable
+app.config['main_game'] = main_game
+
+# set the read_config function in the flask global variable
+app.config['read_config'] = read_config
+
+# set the check_token function in the flask global variable
+app.config['check_token'] = check_token
+
 
 # read the config file
 config = read_config.read_config()
 
 # register the blueprints
+
+# import blueprints
+from blueprints.index import index
+from blueprints.get_token import login
+from blueprints.initial_troops import init_troop
+from blueprints.ready import ready
+
+
+## a blueprint for the test server
 app.register_blueprint(index)
+
+## a blueprint for the login API(get token, player_id, public_key, port for the client)
 app.register_blueprint(login)
+
+## a blueprint for the ready API
+app.register_blueprint(ready)
+
+## a blueprint for the initial troops API
+app.register_blueprint(init_troop)
+
 
 
 if __name__ == "__main__":
