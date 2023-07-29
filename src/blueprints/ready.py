b95dfc7a815ba9c09445299c0dbb8c88e2c5d827
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-07-29 11:28:48 +0330

    add ready.py so the clients can show they are ready for the start of the game

---
+++ src/blueprints/ready.py
@@ -0,0 +1,30 @@
+"""
+in this API client shows that it's ready to start the game 
+that means it has a server on the port that it said in the login API
+"""
+
+from flask import Blueprint
+from flask import current_app
+from flask import jsonify
+
+
+# get the main_game instance from the flask global variable
+main_game = current_app.config['main_game']
+
+# initialize the login blueprint
+ready = Blueprint('ready', __name__)
+
+
+
+@ready.route('/ready', methods=['GET'])
+@current_app.config['check_token'].token_required
+def ready_func(player_id):
+    try:
+        main_game.players[player_id].ready = True
+        output_dict = {"message": "every thing is ok, you should wait for other players to be ready"}
+        main_game.check_all_players_ready()
+        return jsonify(output_dict), 200
+
+    except:
+        output_dict = {"error": "this player_id doesn't exist"}
+        return jsonify(output_dict), 404
