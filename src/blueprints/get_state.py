b085a9ba02081d5df11ca2c4aa70ca4194e3e19c
Zahra Tavakoli <z.tavakoli@ec.iut.ac.ir>
2023-08-06 17:59:19 +0330

    Add get_state API

---
+++ src/blueprints/get_state.py
@@ -0,0 +1,17 @@
+
+from flask import Blueprint , jsonify , current_app 
+from components.game import Game
+
+from tools.check_token import token_required
+from tools.check_player import check_player
+
+
+get_state = Blueprint('get_state',__name__)
+
+main_game = current_app.config['main_game']
+
+@get_state.route('/get_state',methods=['GET'])
+@token_required
+@check_player
+def get_state_func():
+    return jsonify(main_game.state),200
