00053e7ea38bcd7b29b4fc541469069a4c777b16
TRX <trx@TRXs-MacBook-Air.local>
2023-08-06 20:09:16 +0330

    add a variable to check if the game is in the state of initilize or turns

--- src/components/game.py
+++ src/components/game.py
@@ -22,6 +22,7 @@ class Game:
         self.state = None # that could be 'add troops': 1, 'attack': 2, 'move troops': 3
         self.player_turn = None # Player object: the player who is playing this turn
         self.game_started = False # True if the game already started
+        self.game_state = 1 # 1: still need to initialize the troops, 2: the game started
 
     def add_player(self, player_id: int) -> None:
         # add a player to the game if it doesn't exist
