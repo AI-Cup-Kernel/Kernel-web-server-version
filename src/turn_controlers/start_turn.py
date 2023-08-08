5db5aa83f95b25e0909d3302be836140b2e61664
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-08 15:37:24 +0330

    make a turn request

--- src/turn_controlers/start_turn.py
+++ src/turn_controlers/start_turn.py
@@ -1,16 +1,15 @@
 from flask import current_app
 import requests
 
-main_game = current_app.config['main_game']
 
-def turn_request(player_id: int) -> None:
+def turn_request(player_id: int, main_game) -> None:
     # this function make a request to player_id to start its turn
     token = main_game.players[player_id].token
     port = main_game.players[player_id].port
     ip = main_game.players[player_id].ip
 
     # make a request to player_id to start its turn
-    url = f'http://{ip}:{port}/start_turn'
+    url = f'http://{ip}:{port}/turn'
     headers = {'x-access-tokens': token}
     response = requests.get(url, headers=headers)
     counter = 1
@@ -19,4 +18,4 @@ def turn_request(player_id: int) -> None:
         counter += 1
         if counter > 10:
             print(f"player{player_id} didn't response")
-            raise Exception(f"player{player_id} didn't response")
+    
