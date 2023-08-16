562984bb472de37a770554fae99bf476976b88f2
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-16 20:59:58 +0330

    add end game and handle the time of turn

--- src/turn_controllers/change_turn.py
+++ src/turn_controllers/change_turn.py
@@ -6,22 +6,37 @@
 
 
 import time
-from turn_controllers.start_turn import turn_request
-from turn_controllers.check_finish import check_finish, game_finished
+from turn_controllers.start_turn import turn_request, end
+from turn_controllers.check_finish import check_finish
+
+
 
 def change_turn(main_game):
     while True:
-        
+        # increase the turn number 
         main_game.turn_number += 1
         print("start turn:", main_game.turn_number)
+
+        # calculate the the player id of the current turn
         player_id = main_game.turn_number % len(main_game.players)
+        
+        # initialize the turn state and player object
         main_game.state = 1
         main_game.player_turn = main_game.players[player_id]
+        
+        # request the player to play
         resp = turn_request(player_id, main_game)
+        # check if the game is finished
         check_finish(main_game)
+
+        # update the game state (state: turn or initial state)
         main_game.update_game_state()
 
+        # wait for the player to play
+        time.sleep(main_game.config["turn_time"])
         if resp == -1:
             continue
-        time.sleep(1)
+
+        # announce the end of the turn to the player
+        end(player_id, main_game)
 
