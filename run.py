2fae5ce96188f3465662d5237121d59191d1e738
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-16 12:12:28 +0330

    after that you just need to run the run.py file without any problem

---
+++ run.py
@@ -0,0 +1,17 @@
+# author: Mohamad Mahdi Reisi
+# Date: 2023/8/16
+
+# Description: This file is used to run the main.py file
+# the reason for this file is that we want to run the main.py file from the root directory of the project
+# so we can use the relative path to import the modules
+
+
+import os 
+
+file_path = os.path.abspath(__file__).split('run.py')[0]
+
+# go to the file path address
+os.chdir(file_path)
+
+# run the main.py file
+os.system('python3 src/main.py')
