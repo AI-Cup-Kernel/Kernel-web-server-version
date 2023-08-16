9aeb7f5c4581b811df7c8fe3d70d3a765aa3f933
TRX <trx@TRXs-MacBook-Air.local>
2023-08-16 15:19:14 +0330

    add get adj matrix

---
+++ src/blueprints/get_adj.py
@@ -0,0 +1,15 @@
+from flask import Blueprint , jsonify , current_app 
+
+
+get_adj = Blueprint('get_adj',__name__) 
+
+main_game = current_app.config['main_game']
+
+@get_adj.route('/get_adj',methods=['GET'])
+@current_app.config['token_required']
+@current_app.config['check_player']
+def get_adj_func(player_id):
+    output_dict = {}
+    for node in main_game.list_of_nodes:
+        output_dict[node.id]= [i.id for i in node.adj_main_map]
+    return jsonify(output_dict), 200
