65a444ec47d847b6a049fd6755056b3823cf0df1
TRX <trx@TRXs-MacBook-Air.local>
2023-08-16 15:01:41 +0330

    add get state API

--- src/blueprints/get_state.py
+++ src/blueprints/get_state.py
@@ -1,8 +1,6 @@
 from flask import Blueprint , jsonify , current_app 
 from components.game import Game
 
-from tools.check_token import token_required
-from tools.check_player import check_player
 
 
 get_state = Blueprint('get_state',__name__)
@@ -10,8 +8,8 @@ get_state = Blueprint('get_state',__name__)
 main_game = current_app.config['main_game']
 
 @get_state.route('/get_state',methods=['GET'])
-@token_required
-@check_player
-def get_state_func():
-    output_dict={'state': main_game.state }
+@current_app.config['token_required']
+@current_app.config['check_player']
+def get_state_func(player_id):
+    output_dict={'state': main_game.state}
     return jsonify(output_dict),200
