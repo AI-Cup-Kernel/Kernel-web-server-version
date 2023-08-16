01239079cedcc116d702579971847fecf87c96df
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-16 13:42:38 +0330

    fix some bugs

--- src/turn_controllers/change_turn.py
+++ src/turn_controllers/change_turn.py
@@ -1,11 +1,20 @@
+# author: Mohamad Mahdi Reisi
+# Date: 2023/8/16
+
+# Description: This file is used to manage the turns of the game
+# it will be called after the game is started
+
+
 import time
 from turn_controllers.start_turn import turn_request
 
 def change_turn(main_game):
     while True:
-        print("start game")
+        print("start turn:", main_game.turn_number)
         player_id = main_game.turn_number % len(main_game.players)
-        turn_request(player_id, main_game)
+        resp = turn_request(player_id, main_game)
         main_game.turn_number += 1
+        if resp == -1:
+            continue
         time.sleep(1)
 
--- src/turn_controllers/start_turn.py
+++ src/turn_controllers/start_turn.py
@@ -1,21 +1,38 @@
-from flask import current_app
+# author: Mohamad Mahdi Reisi
+# Date: 2023/8/16
+
+# Description: This file is used to make a request to player_id to start its turn
+
 import requests
 
 
 def turn_request(player_id: int, main_game) -> None:
     # this function make a request to player_id to start its turn
+    # return 1 if the request was successful
+    # return -1 if the request was unsuccessful
+
     token = main_game.players[player_id].token
     port = main_game.players[player_id].port
     ip = main_game.players[player_id].ip
 
     # make a request to player_id to start its turn
     url = f'http://{ip}:{port}/turn'
-    headers = {'x-access-tokens': token}
-    response = requests.get(url, headers=headers)
+    headers = {'x-access-token': token}
+    try:
+        response = requests.get(url, headers=headers)
+    except:
+        print(f"player{player_id} didn't response")
+        return -1
+    
     counter = 1
     while response.status_code != 200:
         response = requests.get(url, headers=headers)
         counter += 1
         if counter > 10:
-            print(f"player{player_id} didn't response")
+            print(f"player{player_id} didn't response")   
+            response = response.json()
+            if 'error' in response:
+                print(response['error'])
+            return -1
+    return 1
     
