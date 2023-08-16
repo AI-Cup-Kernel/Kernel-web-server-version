df5144e9365aa64e24581a0a0152b596a4e804e4
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-16 15:01:25 +0330

    add get turn number API

--- src/blueprints/get_turn_number.py
+++ src/blueprints/get_turn_number.py
@@ -10,8 +10,8 @@ get_turn_number = Blueprint('get_turn_number',__name__)
 main_game = current_app.config['main_game']
 
 @get_turn_number.route('/get_turn_number',methods=['GET'])
-@token_required
-@check_player
-def get_turn_number_func():
+@current_app.config['token_required']
+@current_app.config['check_player']
+def get_turn_number_func(player_id):
     output_dict={'turn_number': main_game.turn_number}
     return jsonify(output_dict),200
