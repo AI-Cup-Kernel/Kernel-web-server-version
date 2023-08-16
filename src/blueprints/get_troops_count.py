6912af348a177eda380c61dfc238678558e56fce
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-16 14:24:18 +0330

    add get troops count API

--- src/blueprints/get_troops_count.py
+++ src/blueprints/get_troops_count.py
@@ -1,10 +1,4 @@
 from flask import Blueprint , jsonify , current_app 
-from components.game import Game
-from components.node import Node
-from components.player import Player
-
-from tools.check_token import token_required
-from tools.check_player import check_player
 
 
 get_troops_count = Blueprint('get_troops_count',__name__) 
@@ -12,10 +6,10 @@ get_troops_count = Blueprint('get_troops_count',__name__)
 main_game = current_app.config['main_game']
 
 @get_troops_count.route('/get_troops_count',methods=['GET'])
-@token_required
-@check_player
-def get_troops_count_func():
+@current_app.config['token_required']
+@current_app.config['check_player']
+def get_troops_count_func(player_id):
     output_dict = {}
     for node in main_game.list_of_nodes:
         output_dict[node.id]=node.number_of_troops
-    return jsonify(output_dict),200
+    return jsonify(output_dict), 200
