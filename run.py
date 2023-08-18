8d57d98afb96948d4ace6c34a72db1475dc07643
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-18 22:33:35 +0330

    add support for different name of python in the cmd/terminal

--- run.py
+++ run.py
@@ -13,5 +13,18 @@ file_path = os.path.abspath(__file__).split('run.py')[0]
 # go to the file path address
 os.chdir(file_path)
 
+
+different_python_name_in_terminal = ['python3', 'python', 'py']
+
 # run the main.py file
-os.system('python3 src/main.py')
+flag = False
+for i in different_python_name_in_terminal:
+    try:
+        os.system(i + ' src/main.py')
+        flag = True
+        break
+    except:
+        continue
+
+if not flag:
+    print('Error: python is not installed on your system or you have not added it to the path')
