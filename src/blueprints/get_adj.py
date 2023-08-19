3f43524403df34ed45d8eeb98c6ab61b9b956333
TRX <trx@TRXs-MacBook-Air.local>
2023-08-19 12:43:53 +0330

    fix the name of the variables

--- src/blueprints/get_adj.py
+++ src/blueprints/get_adj.py
@@ -10,6 +10,6 @@ main_game = current_app.config['main_game']
 @current_app.config['check_player']
 def get_adj_func(player_id):
     output_dict = {}
-    for node in main_game.list_of_nodes:
+    for node in main_game.nodes.values():
         output_dict[node.id]= [i.id for i in node.adj_main_map]
     return jsonify(output_dict), 200
