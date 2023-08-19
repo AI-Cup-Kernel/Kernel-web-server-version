609a164494e7ea4be0d4c4f5f776ca15e428b8c2
TRX <trx@TRXs-MacBook-Air.local>
2023-08-19 11:10:15 +0330

    fix so many bugs

--- src/blueprints/get_strategic_nodes.py
+++ src/blueprints/get_strategic_nodes.py
@@ -11,5 +11,5 @@ main_game = current_app.config['main_game']
 @current_app.config['token_required']
 @current_app.config['check_player']
 def get_strategic_nodes_func(player_id):
-    output_dict={'strategic_nodes': [i.id for i in main_game.nodes if i.is_strategic]}
+    output_dict={'strategic_nodes': [i.id for i in main_game.nodes.values() if i.is_strategic]}
     return jsonify(output_dict),200
