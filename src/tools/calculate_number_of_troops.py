d76fc48040b96f21505e7186a3bab1e2f66d6fc3
TRX <trx@TRXs-MacBook-Air.local>
2023-08-19 15:39:26 +0330

    add different timing for init and turn, correct the formual to calcualte the number of troops

--- src/tools/calculate_number_of_troops.py
+++ src/tools/calculate_number_of_troops.py
@@ -1,3 +1,5 @@
 def calculate_number_of_troops(player_id, main_game):
     # calculate the number of troops to give a player at the beginning of a turn
-    return 1
+    number_of_nodes = len(main_game.player_turn.nodes)
+    score_of_strategic_nodes = sum([i.score_of_strategic for i in main_game.player_turn.nodes if i.is_strategic])
+    return (number_of_nodes-9) // 3 + score_of_strategic_nodes
