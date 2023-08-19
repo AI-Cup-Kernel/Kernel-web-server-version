ae26aa9e13278bb59a78dde14e41cf1b8aaffc18
TRX <trx@TRXs-MacBook-Air.local>
2023-08-19 10:16:46 +0330

    fix some bugs and add some comments

--- src/turn_controllers/start_turn.py
+++ src/turn_controllers/start_turn.py
@@ -1,16 +1,17 @@
 # author: Mohamad Mahdi Reisi
 # Date: 2023/8/16
 
-# Description: This file is used to make a request to player_id to start its turn
+# Description: This file is used to make a request to player_id to start its turn or end its turn
 
 import requests
 
 
-def turn_request(player_id: int, main_game) -> None:
+def start_turn_request(player_id: int, main_game) -> None:
     # this function make a request to player_id to start its turn
-    # return 1 if the request was successful
-    # return -1 if the request was unsuccessful
+    ## return 1 if the request was successful
+    ## return -1 if the request was unsuccessful
 
+    # define some variables
     token = main_game.players[player_id].token
     port = main_game.players[player_id].port
     ip = main_game.players[player_id].ip
@@ -25,32 +26,54 @@ def turn_request(player_id: int, main_game) -> None:
     try:
         response = requests.get(url, headers=headers, timeout=main_game.config["timeout"])
         if response.status_code != 200:
-            print(response['error'])
+            if 'error' not in response:
+                print("Unknown error")
+                if main_game.debug:
+                    main_game.print("Unknown error")
+            else:
+                print(response['error'])
+                if main_game.debug:
+                    main_game.print(response['error'])
     except:
         print(f"player{player_id} didn't response")
+        if main_game.debug:
+            main_game.print(f"player{player_id} didn't response")
         return -1
     
 
     return 1
     
 
-def end(player_id, main_game):
+def end_turn_request(player_id, main_game):
     # this function make a request to player_id to announce the end of its turn
+    ## return 1 if the request was successful
+    ## return 0 if the request was unsuccessful
+
+    # define some variables
     token = main_game.players[player_id].token
     port = main_game.players[player_id].port
     ip = main_game.players[player_id].ip
 
     # make a request to player_id to start its turn
-
     url = f'http://{ip}:{port}/end'
 
     headers = {'x-access-token': token}
     try:
         response = requests.get(url, headers=headers, timeout=main_game.config["timeout"])
         if response.status_code != 200:
-            print(response['error'])
+            if 'error' not in response:
+                print("Unknown error")
+                if main_game.debug:
+                    main_game.print("Unknown error")
+            else:
+                print(response['error'])
+                if main_game.debug:
+                    main_game.print(response['error'])
     except:
         print(f"player{player_id} didn't response")
+        if main_game.debug:
+            main_game.print(f"player{player_id} didn't response")
         return -1
+    
 
     return 1
