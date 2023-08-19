2b42ddee13085f9ff0b4cc9b46e56b2ab395aa51
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-19 15:15:16 +0330

    add score to the strategic nodes

--- src/tools/calculate_number_of_troops.py
+++ src/tools/calculate_number_of_troops.py
@@ -1,2 +1,3 @@
-def calculate_number_of_troops(player, main_game):
-    return len(player.nodes)
+def calculate_number_of_troops(player_id, main_game):
+    # calculate the number of troops to give a player at the beginning of a turn
+    return 1
