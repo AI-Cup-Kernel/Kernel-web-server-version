2701e520f1508b876e5f91dad9ce5e0f2aeebf73
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-20 20:26:04 +0330

    edit

--- src/tools/check_player.py
+++ src/tools/check_player.py
@@ -8,7 +8,7 @@ def check_player(func):
     @wraps(func)
     def decorator(player_id):   
         if player_id != main_game.player_turn.id:
-            return jsonify({"error":"It's not your turn"}),403
+            return jsonify({"error":"It's not your turn"}),410
              
         return func(player_id)
     
