184f60db2df6a6fce44835aaaf4655a5bddb5135
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-20 20:07:10 +0330

    fix a bug

--- src/tools/check_player.py
+++ src/tools/check_player.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import current_app
-
+from flask import jsonify
 
 main_game = current_app.config['main_game']
 
@@ -8,7 +8,7 @@ def check_player(func):
     @wraps(func)
     def decorator(player_id):   
         if player_id != main_game.player_turn.id:
-            return "You are not allowed to do this action"
+            return jsonify({"message":"It's not your turn"}),403
              
         return func(player_id)
     
