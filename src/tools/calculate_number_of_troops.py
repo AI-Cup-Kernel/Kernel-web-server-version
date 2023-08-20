1b06bfaf63e75135fe8c430b9f3a1a4e686ffdb3
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-20 23:11:39 +0330

    fix bug negetive troop

--- src/tools/calculate_number_of_troops.py
+++ src/tools/calculate_number_of_troops.py
@@ -2,4 +2,6 @@ def calculate_number_of_troops(player_id, main_game):
     # calculate the number of troops to give a player at the beginning of a turn
     number_of_nodes = len(main_game.player_turn.nodes)
     score_of_strategic_nodes = sum([i.score_of_strategic for i in main_game.player_turn.nodes if i.is_strategic])
+    if number_of_nodes < 9:
+        return score_of_strategic_nodes
     return (number_of_nodes-9) // 3 + score_of_strategic_nodes
