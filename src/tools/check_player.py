cd154a3a9b35109ecc9efa85978188b034ba7b53
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-22 10:45:00 +0330

    fix requesting before ready request

--- src/tools/check_player.py
+++ src/tools/check_player.py
@@ -6,7 +6,10 @@ main_game = current_app.config['main_game']
 
 def check_player(func):
     @wraps(func)
-    def decorator(player_id):   
+    def decorator(player_id): 
+        if main_game.game_started == False:
+            return jsonify({"error":"The game didn't start yet"}),400
+          
         if player_id != main_game.player_turn.id:
             return jsonify({"error":"It's not your turn"}),403
              
