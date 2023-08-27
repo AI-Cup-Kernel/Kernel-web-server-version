ab25a1e897e6622613b5bb75bc37c45073012eb5
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-27 16:44:44 +0330

    correct spelling

--- src/blueprints/move_troop.py
+++ src/blueprints/move_troop.py
@@ -92,9 +92,9 @@ def move_troop_func(player_id):
     if troop_count <= 0:
         return jsonify({'error':'troop_count should be positive'}),400
 
-    #check if the sourse and destination isn't same
+    #check if the source and destination isn't same
     if source == destination:
-        return jsonify({'error':'source ans=d destiantion should be diffrent'})
+        return jsonify({'error':'source and destination should be different'})
     
     main_game.nodes[source].number_of_troops -= troop_count
     main_game.nodes[destination].number_of_troops += troop_count
