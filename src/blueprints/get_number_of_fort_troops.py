db74f50323c16ec6612fab2db42736e2a33d8775
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-31 15:58:07 +0330

    add comment

--- src/blueprints/get_number_of_fort_troops.py
+++ src/blueprints/get_number_of_fort_troops.py
@@ -9,6 +9,7 @@ main_game = current_app.config['main_game']
 @current_app.config['token_required']
 @current_app.config['check_player']
 def get_number_of_fort_troops_func(player_id):
+    # this API used to get the number of fort troops on each node
     output_dict = {}
     for node in main_game.nodes.values():
         output_dict[node.id]=node.number_of_fort_troops
